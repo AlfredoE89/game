bcab701c3ebb23189a33501bd08d4871593ada0b
AlfredoE89 <w0lf_0f_wall_street@icloud.com>
2019-10-03 20:14:22 -0500

    updated game/color change

---
+++ src/js/main.js
@@ -0,0 +1,14 @@
+var game = (function(){})();
+var game = (function(){
+    var canvas = document.getElementById('canvas');
+    var ctx = canvas.getContext('2d');
+  
+    return {
+      init: function(){
+        canvas.height = 600;
+        canvas.width = 800;
+      }
+    }
+  })();
+  
+  game.init();
